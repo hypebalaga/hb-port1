d3de19811c7d43dadcedfc880291d295f06d2fad
santhnayak <santhnayak@deloitte.com>
2023-06-24 20:54:34 +0530

    personal_project

--- hb/hb/settings.py
+++ hb/hb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-!mnkz3mcjd4r%ds8g3p_r70_d&0klit#s!dwzlx0jb*&2r31uc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
