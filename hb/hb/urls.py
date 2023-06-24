231f5eb1e073241a7c7d40bb33f7806a1aae3522
hypebalaga <hypebalaga@gmail.com>
2023-06-25 00:53:57 +0530

    number

--- hb/hb/urls.py
+++ hb/hb/urls.py
@@ -27,7 +27,7 @@ urlpatterns = (
     [
         path("admin/", admin.site.urls),
         path("", include("api.urls")),
-        path('wa.me/+91944835656', lambda request: redirect('https://wa.me/+91944835656'))
+        path('wa.me/+919448835656', lambda request: redirect('https://wa.me/+919448835656'))
     ]
     + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
